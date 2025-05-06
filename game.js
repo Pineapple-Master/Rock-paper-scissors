6fbd72ed8ca66c15d634ab84e0ec830925b685a7
Atharv Elamsenthil <68880424+Pineapple-Master@users.noreply.github.com>
2025-05-06 04:45:05 -0400

    Added more stuff

--- game.js
+++ game.js
@@ -3,6 +3,9 @@
 let userChoice;
 let compChoice;
 
+
+
+
 function computer_choose() {
     let numChoice = Math.floor(Math.random() * 3);
     console.log(numChoice);
@@ -21,7 +24,7 @@ function user_choose(){
     let temp_choice;
     while(!valid_choice){
         temp_choice = prompt("Please enter: Rock, Paper, or Scissors");
-        if(temp_choice == "Paper", temp_choice == "Scissors", temp_choice == "Rock") {
+        if(temp_choice == "Paper" || temp_choice == "Scissors" || temp_choice == "Rock") {
             valid_choice = true;
             return 
         } else {
@@ -31,4 +34,31 @@ function user_choose(){
 }
 
 
-console.log(computer_choose());
+const start_btn = document.querySelector("#start-btn");
+if (start_btn) {
+    start_btn.addEventListener("click", () => {
+        window.location.href = "game.html";
+    });
+}
+
+const rock_choice = document.querySelector("#rock");
+if(rock_choice){
+    rock_choice.addEventListener("click", () => {
+        alert("rock");
+    });
+}
+const paper_choice = document.querySelector("#paper");
+if(paper_choice){
+    paper_choice.addEventListener("click", () => {
+        alert("paper");
+    });
+}
+const scissors_choice = document.querySelector("#scissors");
+if(scissors_choice){
+    scissors_choice.addEventListener("click", () => {
+        alert("scissors");
+    });
+}
+
+
+
